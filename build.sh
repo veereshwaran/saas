d452a1f4b45552b393c975f545d475ac9bb5e18c
Veeresh <veereshwaran@appranix.com>
2017-11-21 18:20:53 +0530

    ddddddd

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+env
+
 export VERSION=$(curl 'http://192.168.0.129:3000/onprem/service/director' -H 'Authorization: Basic aW5LeUJHZTJ2bXlrTWI0UzVELUI=')
 
 docker build --build-arg VERSION=$VERSION -t dir .
