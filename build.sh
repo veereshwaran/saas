a8f71e76903c922791ddc5c8d3049c3c8f6d8499
Veeresh <veereshwaran@appranix.com>
2017-11-22 10:40:49 +0530

    eeeeeee

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 env
 
-export VERSION=$(curl 'http://192.168.0.129:3000/onprem/service/director' -H 'Authorization: Basic aW5LeUJHZTJ2bXlrTWI0UzVELUI=')
+export HUB=$(curl 'http://192.168.0.129:3000/onprem/service/director' -H 'Authorization: Basic aW5LeUJHZTJ2bXlrTWI0UzVELUI=')
 
-docker build --build-arg VERSION=$VERSION -t dir .
+docker build --build-arg BASE_IMAGE=$HUB -t dir .
