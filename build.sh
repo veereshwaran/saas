cf26034f07782ee53a8db7441ae303b9bc5fd87c
Veeresh <veereshwaran@appranix.com>
2017-11-22 11:29:42 +0530

    kkkkk

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@ ls
 
 export HUB=$(curl "http://${bamboo_APPRANIX_AUTH_TOKEN}@192.168.0.129:3000/onprem/service/director")
 
-tar -cJf circuit-saasaas-1.tar.xz . --exclude='circuit-saasaas-1.tar.xz'
-
-ls
+tar -cJf circuit-saas-1.tar.xz . --exclude='circuit-saas-1.tar.xz'
 
 docker build --build-arg BASE_IMAGE=$HUB -t dir:${bamboo_buildNumber} .
 
