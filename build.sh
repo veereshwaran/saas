33491345a8e25c06704ff1a13f1e33cbf0105f75
Veeresh <veereshwaran@appranix.com>
2017-11-22 10:55:59 +0530

    ggggggg

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 
 export HUB=$(curl 'http://192.168.0.129:3000/onprem/service/director' -H 'Authorization: Basic aW5LeUJHZTJ2bXlrTWI0UzVELUI=')
 
-docker build --build-arg BASE_IMAGE=$HUB -t dir .
+docker build --build-arg BASE_IMAGE=$HUB -t dir:${bamboo_buildNumber} .
